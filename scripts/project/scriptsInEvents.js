cf5a8e037503a2932e6f37edaaa7d13a872591a6
HomeSani <52744389+HomeSani@users.noreply.github.com>
2022-01-21 17:34:19 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+		async EventSheet1_Event3(runtime, localVars)
+		{
+			vkBridge.send("VKWebAppGetAds", {});
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
